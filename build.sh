d0910fffcad379cf6e7d2a23999302e5990eaef4
Matt Broussard <matt@mattb.io>
2022-04-15 01:44:12 -0700

    add webstore assets

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 
 VERSION=$(jq -r .version manifest.json)
 mkdir -p bin
-zip -vr bin/ef-utils-$VERSION.zip lib *.js *.mjs *.html *.json *.css *.png
+zip -vr bin/ef-utils-$VERSION.zip lib *.js *.mjs *.html *.json *.css icon.png
